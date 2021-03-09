379cf42411a04945d18bd59080002417ca908591
Kanin Kongchatree <works.kanink@gmail.com>
2021-03-09 07:35:55 +0700

    Update  variable and function name to be more readable

--- Setting.py
+++ Setting.py
@@ -1,7 +1,7 @@
 #Get credential from enviroment variable
-clientId = ''
-clientSecret = ''
-SITE_URL = 'https://app-apac.onetrust.com'
+client_id = ''
+client_secret = ''
+site_url = 'https://app-apac.onetrust.com'
 
 #CSV File Enviroment Variable
-File_Name = 'msc_apidemo_purpose.csv'
+consent_file_name = 'msc_apidemo_purpose.csv'
